b5352b6430b7c7b1881792fb9941d0d9543a328d
metehansargin <msargin1986@gmail.com>
2026-02-23 01:40:49 +0300

    package and dependency added

--- src/main/java/com/metehansargin/jwt/JwtApplication.java
+++ src/main/java/com/metehansargin/jwt/JwtApplication.java
@@ -2,7 +2,13 @@ package com.metehansargin.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = {"com.metehansargin"})
+@ComponentScan(basePackages = {"com.metehansargin"})
+@EnableJpaRepositories(basePackages = {"com.metehansargin"})
 @SpringBootApplication
 public class JwtApplication {
 
---
+++ src/main/java/com/metehansargin/jwt/entity/User.java
@@ -0,0 +1,47 @@
+package com.metehansargin.jwt.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
